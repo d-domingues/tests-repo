cafc172ee75f29685def1dd6fdcc2876444c86e6
David Filipe Domingues <david.lopes.domingues@everis.com>
2020-06-27 14:47:22 +0100

    rxjs exe1 done

--- src/app/components/rxjs-exercices/rxjs-exercices.component.ts
+++ src/app/components/rxjs-exercices/rxjs-exercices.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
-import { interval } from 'rxjs';
-import { map, repeat, take } from 'rxjs/operators';
+import { merge, Subject, timer } from 'rxjs';
+import { map, mapTo } from 'rxjs/operators';
 
 @Component({
   templateUrl: './rxjs-exercices.component.html',
   styleUrls: ['./rxjs-exercices.component.scss'],
 })
 export class RxjsExercicesComponent {
-  source = interval(1000).pipe(
-    map((next) => 3 - next),
-    take(4),
-    repeat()
+  onClick = new Subject();
+
+  counter = 0;
+
+  source = merge(this.onClick.pipe(mapTo(-1)), timer(0, 1000)).pipe(
+    map((x) => {
+      return {
+        value: 3 - ((this.counter = x === -1 ? 3 : ++this.counter) % 4),
+      };
+    })
   );
+
+  getClass({ value }) {
+    return {
+      'countdown-frame': true,
+      light: value === 0,
+    };
+  }
 }
